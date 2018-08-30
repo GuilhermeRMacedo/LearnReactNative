fde70e59d7acd2a66d0881f8a0b52d42d4ade2c1
Guilherme Macedo <guilhermericardord@gmail.com>
2018-08-30 14:14:23 -0300

    setting up the redurecs pt2

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,28 +1,43 @@
 import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from '../actions/actionTypes'
 
+import placeImage from '../../assets/Mont-Saint-Michel-FRA.jpg'
+
 const initialState = {
     places: [],
     selectedPlace: null
 }
 
 const reducer = (state = initialState, action) => {
-    switch(action.type) {
-            case ADD_PLACE:
-                return {
-                    
-                };
-            case DELETE_PLACE:
-                return {
-
-                };
-            case SELECT_PLACE: 
-                return {
-
-                };
-            case DESELECT_PLACE:
-                return {
-                    
-                }
+    switch (action.type) {
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: Math.random().toString(),
+                    name: action.placeName,
+                    image: placeImage
+                })
+            };
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key
+                }),
+                selectedPlace: null
+            };
+        case SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => {
+                    return place.key === action.placeKey;
+                })
+            };
+        case DESELECT_PLACE:
+            return {
+                ...state, 
+                selectedPlace: null
+            }
         default:
             return state;
     }
