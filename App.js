15556cee68a2373d0c975002fc061ce595ff1f25
Guilherme Macedo <guilhermericardord@gmail.com>
2018-10-02 13:43:16 -0300

    Connecting Screens to Redux

--- App.js
+++ App.js
@@ -11,7 +11,7 @@
 // import placeImage from './src/assets/Mont-Saint-Michel-FRA.jpg'
 
 // class App extends React.Component {
- 
+
 //   placeNameChangedHandler = (text) => {
 //     this.props.onNameChangedPlace(text)
 //   }
@@ -86,15 +86,36 @@
 // export default connect(mapsStateToPorps, mapDispatchToProps)(App);
 
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
 
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace'
 import FindPlaceScreen from './src/screens/FindPlace/Findplace'
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
 
 //register screens
-Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
-Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen);
-Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen);
+Navigation.registerComponent(
+  "awesome-places.AuthScreen",
+  () => AuthScreen,
+  store,
+  Provider
+);
+
+Navigation.registerComponent(
+  "awesome-places.SharePlaceScreen", 
+  () => SharePlaceScreen, 
+  store, 
+  Provider
+);
+
+Navigation.registerComponent(
+  "awesome-places.FindPlaceScreen", 
+  () => FindPlaceScreen, 
+  store, 
+  Provider
+);
 
 
 //start a app
